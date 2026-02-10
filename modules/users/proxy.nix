32c82ce27f19b5f35f6824cf9e0071a792c1ef06
Wolfhard Prell <mail@wolfhard.net>
2026-02-10 13:24:07 +0100

    added OpenClaw (Proxy) service

--- modules/users/proxy.nix
+++ modules/users/proxy.nix
@@ -5,4 +5,23 @@
     extraGroups = ["networkmanager" "wheel" "docker"];
     shell = pkgs.zsh;
   };
+
+  systemd.services.openclaw = {
+    description = "OpenClaw Gateway";
+    wantedBy = ["multi-user.target"];
+    after = ["network.target"];
+
+    serviceConfig = {
+      Type = "simple";
+      User = "proxy";
+      WorkingDirectory = "/home/proxy";
+      ExecStart = "/home/proxy/.npm-global/bin/openclaw gateway";
+      Restart = "always";
+      RestartSec = "10s";
+      Environment = [
+        "HOME=/home/proxy"
+        "PATH=/home/proxy/.npm-global/bin:/run/current-system/sw/bin"
+      ];
+    };
+  };
 }
